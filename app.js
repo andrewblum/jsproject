197b8ced2d000f56488b30246ed68f963eee163f
Andrew <andrewdblum@gmail.com>
2018-02-13 09:18:51 -0800

    heroku deploy

--- app.js
+++ app.js
@@ -1,33 +1,76 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 // const http = require('http').Server(app)
-const path = require('path')
-const fetch = require('node-fetch')
-const PORT = 8000
+const path = require('path');
+const fetch = require('node-fetch');
+const PORT = process.env.PORT || 8000;
+const countries = require('country-list')();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
-app.get('/fighters', (req, res) => {
-  let results
-  fetch('http://ufc-data-api.ufc.com//api/v1/us/fighters')
+//get top 50 artist objects for a genre (tag)
+app.get('/genre/:genre', (req, res) => {
+  let results;
+  fetch(`http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=${req.params.genre}&api_key=0d3ece2f1080c69c00cf22e84789d7a3&format=json`)
     .then(function(response) {
         return response.text();
     }).then(function(body) {
-      results = JSON.parse(body)
-        console.log(typeof body);
-        console.log(body.length);
-        console.log(JSON.parse(body)[0]);
-        res.send(results)
+        results = JSON.parse(body);
+        // console.log(results.topartists.artist);
+        // console.log(typeof body);
+        // console.log(body.length);
+        // console.log(JSON.parse(body)[0]);
+        res.send(results);
     });
+});
 
-})
+//get top 50 artist of every country
+app.get('/country', (req, res) => {
+  let topArtistByCountry = {};
+  let result;
+  Object.keys(countries.getNameList()).forEach(country => {
+    topArtistByCountry[country] = topBandsForCountry(country);
+    console.log(topBandsForCountry(country));
+  });
+  res.send(topArtistByCountry);
+});
+
 
 app.listen(PORT, () => {
   console.log(__dirname);
-  console.log(`listening on ${PORT}`)
-})
+  console.log(`listening on ${PORT}`);
+});
+
+
+
+
+//utils
+
+const showsFromNames = (obj) => {
+
+};
+
+const artistNames = (arr) => {
+  let artistsNames = [];
+  arr.forEach(artist =>
+    artistNames.push(artist.name)
+  );
+  return artistsNames;
+};
+
+const topBandsForCountry = (country) => {
+  let result;
+  fetch(`http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=0d3ece2f1080c69c00cf22e84789d7a3&format=json`)
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(body) {
+      result = JSON.parse(body);
+    });
+  return result;
+};
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,141 @@
+import Chart from 'chart.js';
+
 document.addEventListener('DOMContentLoaded', () => {
+  var ctx = document.getElementById('canvas').getContext('2d');
+  window.myChart = new Chart(ctx, {
+    type: 'bubble',
+    data: bubbleChartData,
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Populairty of genre by region based on # of shows'
+      },
+      tooltips: {
+        mode: 'point'
+      }
+    }
+  });
+
+  document.getElementById('metal').addEventListener('click', function() {
+    bubbleChartData.datasets.forEach(function(dataset) {
+      dataset.data = dataset.data.map(function() {
+        return {
+          x: randomScalingFactor(),
+          y: randomScalingFactor(),
+          r: Math.abs(randomScalingFactor()) / 5,
+        };
+      });
+    });
+    window.myChart.update();
+  });
+
+});
 
 
 
 
-})
+
+
+
+
+
+
+
+
+
+
+
+window.chartColors = {};
+window.chartColors.red = 'rgba(255, 99, 132, 0.2)';
+window.chartColors.orange = 'rgba(200, 199, 132, 0.2)';
+
+
+
+
+
+
+
+const randomScalingFactor = function() {
+  return Math.random() * 100;
+};
+
+
+
+var DEFAULT_DATASET_SIZE = 7;
+var addedCount = 0;
+var color = Chart.helpers.color;
+var bubbleChartData = {
+	animation: {
+		duration: 10000
+	},
+	datasets: [{
+		label: 'Data 1 thing',
+		backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+		borderColor: window.chartColors.red,
+		borderWidth: 1,
+		data: [{
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}]
+	}, {
+		label: 'Data 2 thing',
+		backgroundColor: color(window.chartColors.orange).alpha(0.5).rgbString(),
+		borderColor: window.chartColors.orange,
+		borderWidth: 1,
+		data: [{
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}, {
+			x: randomScalingFactor(),
+			y: randomScalingFactor(),
+			r: Math.abs(randomScalingFactor()) / 5,
+		}]
+	}]
+};
